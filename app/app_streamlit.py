85f25fce2be2dacaa6dd2e6d8ea90c93d5802029
zcc126 <58626340+zcc126@users.noreply.github.com>
2022-07-21 20:22:42 +0200

    Update app_streamlit.py

--- app/app_streamlit.py
+++ app/app_streamlit.py
@@ -2,6 +2,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import os
 
+key_blob=os.environ["KEY_BLOB"]
 
-st.title('Hello in my first streamlit App :)')
+st.title('Hello in my first streamlit App :)' + key_blob)
