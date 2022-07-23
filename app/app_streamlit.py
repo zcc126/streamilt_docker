0a4f5bd19c792154be4bce0acf0d19dc3d8b8405
zcc126 <58626340+zcc126@users.noreply.github.com>
2022-07-23 22:45:50 +0200

    Update app_streamlit.py

--- app/app_streamlit.py
+++ app/app_streamlit.py
@@ -1,8 +1,8 @@
-# Import streamlit 
+
 import streamlit as st
 import pandas as pd
 import numpy as np
-import os
+
 f = open("/etc/keys_azure/keyblob", "r")
 key_blob=f.read()
 st.title('Hello in my first streamlit App :)' + str(key_blob))
