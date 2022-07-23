2d1a082aeb03587489686a84ca7ab42304d80d3a
zcc126 <58626340+zcc126@users.noreply.github.com>
2022-07-23 23:38:11 +0200

    Update app_streamlit.py

--- app/app_streamlit.py
+++ app/app_streamlit.py
@@ -3,21 +3,20 @@ import pandas as pd
 import numpy as np
 import os
 from azure.storage.blob import BlobServiceClient
-import time
+from io import StringIO
 
 key_blob=os.environ["KEY_BLOB"]
 url_blob=os.environ["URL_BLOB"]
 local_file_name="Airlines.csv"
 container_name="airline"
 
-# st.title('Hello in my first streamlit App :)')
+st.title('Hello in my first streamlit App :)')
 
-t1=time.time()
+
 blob_service_client_instance = BlobServiceClient(account_url=url_blob, credential=key_blob)
-blob_client_instance = blob_service_client_instance.get_blob_client(container_name, local_file_name, snapshot=None)
-t2=time.time()
-
-st.title('Hello in my first streamlit App :)' + str(t2-t1))
+# blob_client_instance = blob_service_client_instance.get_blob_client(container_name, local_file_name, snapshot=None)
+blobstring = blob_client_instance.get_blob_to_text(container_name, local_file_name)
+dataframe_blobdata = pd.read_csv(StringIO(blobstring))
 
 # dataframe_blobdata = pd.read_csv(local_file_name)
-# st.dataframe(dataframe_blobdata.head(5))
+st.dataframe(dataframe_blobdata.head(5))
